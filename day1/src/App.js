e2275396edea81bee06b51821c04cf139afc32ce
Huzaif2004 <124855224+Huzaif2004@users.noreply.github.com>
2023-11-16 15:37:59 +0530

    Add files via upload

---
+++ day1/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+import HelloWorld from './classComponents/democlass';
+import Hello from './functionalComponent/Hello';
+function App() {
+  return (
+    <div className="App" >
+     
+      <HelloWorld/>
+     <Hello/> 
+     
+    </div>
+  );
+}
+
+export default App;
